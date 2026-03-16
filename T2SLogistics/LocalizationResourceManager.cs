9a1b114a3787fc0f506cd1c07db04894fc57f7f3
dmdulla <88859154+dmdulla@users.noreply.github.com>
2026-03-16 17:34:04 +0000

    DMD 20260316 Localization ignored

---
+++ T2SLogistics/LocalizationResourceManager.cs
@@ -0,0 +1,34 @@
+﻿using T2SLogistics.Resx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2SLogistics
+{
+    public class LocalizationResourceManager : INotifyPropertyChanged
+    {
+        private LocalizationResourceManager()
+        {
+            AppResources.Culture = CultureInfo.CurrentCulture;
+        }
+
+        public static LocalizationResourceManager Instance { get; } = new();
+
+        public string this[string resourceKey]
+            => AppResources.ResourceManager.GetString(resourceKey, AppResources.Culture) ?? $"[{resourceKey}]";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetCulture(CultureInfo culture)
+        {
+            AppResources.Culture = culture;
+
+            // Notify all bindings to update
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+    }
+}
